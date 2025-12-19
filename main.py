4c05f527473ded8f1cccf82a6a806f2a3a5e0447
cololaborde <cololaborde@gmail.com>
2025-12-18 21:55:22 -0300

    flexible full path match

--- main.py
+++ main.py
@@ -24,8 +24,8 @@ if __name__ == "__main__":
             if matched_chapter_number_srt:
                 chapter_number_srt = matched_chapter_number_srt.group(2)
                 if chapter_number == chapter_number_srt:
-                    str_full_path = f"{directory}/{srt_filename}"
-                    video_full_path = f"{directory}/{video_filename}"
+                    str_full_path = f"{directory}{"/" if not directory.endswith("/") else ""}{srt_filename}"
+                    video_full_path = f"{directory}{"/" if not directory.endswith("/") else ""}{video_filename}"
                     extension = f".{video_filename.split('.')[-1]}"
                     new_str_full_path = f"{video_full_path.replace(extension, '.srt')}"
                     print(str_full_path)
