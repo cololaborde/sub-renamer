11d6b1a2b0d18506ed6be9277b22b760f7b4b392
cololaborde <cololaborde@gmail.com>
2025-12-19 10:31:10 -0300

    increase performant removing one iteration

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ from re import search as re_search
 VIDEO_EXTENSIONS = [".mp4", ".mkv", ".avi", ".mov", ".wmv", ".flv", ".webm"]
 
 if __name__ == "__main__":
-    srt_filenames = []
-    video_filenames = []
+    srt_filenames = {}
+    video_filenames = {}
+    match_re = r'[sS]?(\d+)[eExX](\d+)'
     
     directory = sys.argv[1] if len(sys.argv) > 1 else None
     if not directory or not os.path.isdir(directory):
@@ -15,29 +16,24 @@ if __name__ == "__main__":
 
     for filename in os.listdir(directory):
         if filename.lower().endswith(".srt"):
-            srt_filenames.append(filename)
-        if filename.lower().endswith(tuple(VIDEO_EXTENSIONS)):
-            video_filenames.append(filename)
+            matched_chapter_number_srt = re_search(match_re, filename)
+            if matched_chapter_number_srt:
+                chapter_number_srt = matched_chapter_number_srt.group(2).strip().lstrip('0')
+                srt_filenames[chapter_number_srt] = filename
 
-    for video_filename in video_filenames:
-        match_re = r'[sS]?(\d+)[eExX](\d+)'
-        matched_chapter_number = re_search(match_re, video_filename)
-        if matched_chapter_number:
-            chapter_number = matched_chapter_number.group(2).strip().lstrip('0')
-        
-        for srt_filename in srt_filenames:
-            matched_chapter_number_srt = re_search(match_re, srt_filename)
-            if not matched_chapter_number_srt:
-                continue
-            
-            chapter_number_srt = matched_chapter_number_srt.group(2).strip().lstrip('0')
-            if chapter_number != chapter_number_srt:
-                continue
-            
-            str_full_path = f"{directory}{"/" if not directory.endswith("/") else ""}{srt_filename}"
-            video_full_path = f"{directory}{"/" if not directory.endswith("/") else ""}{video_filename}"
-            extension = f".{video_filename.split('.')[-1]}"
-            new_str_full_path = f"{video_full_path.replace(extension, '.srt')}"
-            print(f"Renaming {str_full_path} to {new_str_full_path}")
-            os.rename(str_full_path, new_str_full_path)
-            break
+        if filename.lower().endswith(tuple(VIDEO_EXTENSIONS)):
+            matched_chapter_number = re_search(match_re, filename)
+            if matched_chapter_number:
+                chapter_number = matched_chapter_number.group(2).strip().lstrip('0')
+                video_filenames[chapter_number] = filename
+
+    for srt_chapter, srt_filename in srt_filenames.items():
+        str_full_path = f"{directory}{"/" if not directory.endswith("/") else ""}{srt_filename}"
+        video_filename = video_filenames.get(srt_chapter)
+        if not video_filename:
+            continue
+        video_full_path = f"{directory}{"/" if not directory.endswith("/") else ""}{video_filename}"
+        extension = f".{video_filename.split('.')[-1]}"
+        new_str_full_path = f"{video_full_path.replace(extension, '.srt')}"
+        print(f"Renaming {str_full_path} to {new_str_full_path}")
+        os.rename(str_full_path, new_str_full_path)
