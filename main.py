200ba79a102992453201d3702ec6c0439a3832c2
cololaborde <cololaborde@gmail.com>
2025-12-24 20:22:03 -0300

    add request confirmation option

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ VIDEO_EXTENSIONS = [".mp4", ".mkv", ".avi", ".mov", ".wmv", ".flv", ".webm"]
 
 if __name__ == "__main__":
     directory = sys_argv[1] if len(sys_argv) > 1 else None
+    preview = sys_argv[2] if len(sys_argv) > 2 and sys_argv[2] == "--preview" else False
+    
     if not directory or not os_path.isdir(directory):
         print("Usage: python main.py <directory>")
         sys_exit(1)
@@ -36,4 +38,10 @@ if __name__ == "__main__":
         extension = f".{video_filename.split('.')[-1]}"
         new_str_full_path = f"{video_full_path.replace(extension, '.srt')}"
         print(f"Renaming {str_full_path} to {new_str_full_path}")
-        os_rename(str_full_path, new_str_full_path)
+        if not preview:
+            os_rename(str_full_path, new_str_full_path)
+        else:
+            user_input = input("Confirm? (y/n): ")
+            if user_input.lower() == "n":
+                continue
+            os_rename(str_full_path, new_str_full_path)
